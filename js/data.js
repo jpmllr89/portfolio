3aca6bc8bd372612f564a6bbcc024783808853af
Jesse Miller <jpmllr89@gmail.com>
2024-01-09 23:36:50 -0500

    update website

--- js/data.js
+++ js/data.js
@@ -16,211 +16,214 @@ const data = {
   //     ]
   //   }
   // ],
-  dataPortfolioCards : [
+  dataPortfolioCards: [
     {
-      picture:"./assets/img/saas.png",
-      title:'Software as a Service',
-      caption:'SaaS Website',
+      picture: "./assets/img/saas.png",
+      title: "Software as a Service",
+      caption: "SaaS Website",
       attributes: [
         {
           name: "data-item",
-          value: "Responsive CSS"
+          value: "Responsive CSS",
         },
         {
           name: "data-open",
-          value: "modal1"
-        }
-      ]
+          value: "modal1",
+        },
+      ],
     },
     {
-      picture: './assets/img/recipes_api.png',
-      title:'Recipe Collection API',
-      caption:'Recipe API',
+      picture: "./assets/img/recipes_api.png",
+      title: "Recipe Collection API",
+      caption: "Recipe API",
       attributes: [
         {
           name: "data-item",
-          value: "Javascript"
+          value: "Javascript",
         },
         {
           name: "data-open",
-          value: "modal2"
-        }
-      ]
+          value: "modal2",
+        },
+      ],
     },
     {
-      picture:'./assets/img/lawfirm.png',
-      title:'Lawfirm Business',
-      caption:'Lawfirm Landing Page',
+      picture: "./assets/img/lawfirm.png",
+      title: "Lawfirm Business",
+      caption: "Lawfirm Landing Page",
       attributes: [
         {
           name: "data-item",
-          value: "Responsive CSS"
+          value: "Responsive CSS",
         },
         {
           name: "data-open",
-          value: "modal3"
-        }
-      ]
+          value: "modal3",
+        },
+      ],
     },
     {
-      picture:'./assets/img/taskApp.png',
-      title:'Super Retro Production',
-      caption:'Productivity App',
+      picture: "./assets/img/taskApp.png",
+      title: "Super Retro Production",
+      caption: "Productivity App",
       attributes: [
         {
           name: "data-item",
-          value: "React"
+          value: "React",
         },
         {
           name: "data-open",
-          value: "modal4"
-        }
-      ]
+          value: "modal4",
+        },
+      ],
     },
     {
-      picture:'./assets/img/youtube.png',
-      title:'Nintendo.com Clone',
-      caption:'A clone from Nintendo\'s website',
+      picture: "./assets/img/youtube.png",
+      title: "Nintendo.com Clone",
+      caption: "A clone from Nintendo's website",
       attributes: [
         {
           name: "data-item",
-          value: "Responsive CSS"
+          value: "Responsive CSS",
         },
         {
           name: "data-open",
-          value: "modal5"
-        }
-      ]
+          value: "modal5",
+        },
+      ],
     },
     {
-      picture:'./assets/img/monstercat.png',
-      title:'Monstercat Music Player',
-      caption:'Responsive Website',
+      picture: "./assets/img/monstercat.png",
+      title: "Monstercat Music Player",
+      caption: "Responsive Website",
       attributes: [
         {
           name: "data-item",
-          value: "Responsive CSS"
+          value: "Responsive CSS",
         },
         {
           name: "data-open",
-          value: "modal6"
-        }
-      ]
+          value: "modal6",
+        },
+      ],
     },
-    // {
-    //   picture:'./assets/img/portfolio-7.jpg',
-    //   title:'UI Design',
-    //   caption:'Cool Design',
-    //   attributes: [
-    //     {
-    //       name: "data-item",
-    //       value: "ui"
-    //     },
-    //     {
-    //       name: "data-open",
-    //       value: "modal7"
-    //     }
-    //   ]
-    // },
-    // {
-    //   picture:'./assets/img/portfolio-8.jpg',
-    //   title:'UI Design',
-    //   caption:'Masterpiece',
-    //   attributes: [
-    //     {
-    //       name: "data-item",
-    //       value: "ui"
-    //     },
-    //     {
-    //       name: "data-open",
-    //       value: "modal8"
-    //     }
-    //   ]
-    // }
+    {
+      picture: "./assets/img/coderlandingpage.png",
+      title: "Coder Landing Page",
+      caption: "Landing Page",
+      attributes: [
+        {
+          name: "data-item",
+          value: "Responsive CSS",
+        },
+        {
+          name: "data-open",
+          value: "modal7",
+        },
+      ],
+    },
+    {
+      picture: "./assets/img/tetris.png",
+      title: "Tetris",
+      caption: "Gaming App",
+      attributes: [
+        {
+          name: "data-item",
+          value: "Javascript",
+        },
+        {
+          name: "data-open",
+          value: "modal8",
+        },
+      ],
+    },
   ],
-  
-  dataModals : [
+
+  dataModals: [
     {
       id: "modal1",
       h3: "Saas Website",
       img: "./assets/img/saas.png",
-      strongParagraph: "Mock Software as a Service Website",
-      firstParagraph: "This is a mock website for a SaaS company. It is a responsive website that is built with HTML, CSS, and JavaScript. It is a single page website that has a smooth scroll effect",
-      link: "https://saas-8b9b.vercel.app/"
+      strongParagraph: "Software as a Service Website",
+      firstParagraph:
+        "This is a website for a SaaS company. It is a responsive website that is built with HTML, CSS, and JavaScript. It is a single page website that has a smooth scroll effect",
+      link: "https://saas-8b9b.vercel.app/",
     },
     {
       id: "modal2",
       h3: "Recipe API",
       img: "./assets/img/recipe_api.png",
       strongParagraph: "Recipe Collection App",
-      firstParagraph: "This is a recipe collection app that uses an API to fetch recipes. It is a single page, responsive website that is built with HTML, CSS, and JavaScript and has a smooth scrolling effect.",
-      link: "https://what-to-eat-api-v2-43z8.vercel.app/"
+      firstParagraph:
+        "This is a recipe collection app that uses an API to fetch recipes. It is a single page, responsive website that is built with HTML, CSS, and JavaScript and has a smooth scrolling effect.",
+      link: "https://what-to-eat-api-v2-43z8.vercel.app/",
     },
     {
       id: "modal3",
       h3: "Small Lawfirm",
       img: "./assets/img/lawfirm.png",
       strongParagraph: "Law Firm Small business landing page",
-      firstParagraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim corrupti repellendus sunt minus, ipsam cumque perspiciatis. Obcaecati, sit eligendi illo ipsa exercitationem modi cumque repellendus eos aliquam magni reiciendis nam?",
-      link: "https://mock-lawfirm.vercel.app/"
+      firstParagraph:
+        "Law Firm Small business landing page. It is a single page, responsive website that is built with HTML, CSS, and JavaScript.",
+      link: "https://mock-lawfirm.vercel.app/",
     },
     {
       id: "modal4",
       h3: "Productivity App",
       img: "./assets/img/taskApp.png",
       strongParagraph: "Super Retro Production",
-      firstParagraph: "This is a productivity app that utilizes the React framework. It is a single page, responsive website that is built with HTML, CSS, and Javascript.  This page also won the Devslopes Hackathon.",
-      link: "https://prod-app-hackathon.vercel.app/"
+      firstParagraph:
+        "This is a productivity app that utilizes the React framework. It is a single page, responsive website that is built with HTML, CSS, and Javascript.  This page also won the Devslopes Hackathon.",
+      link: "https://prod-app-hackathon.vercel.app/",
     },
     {
       id: "modal5",
       h3: "Nintendo Clone",
       img: "./assets/img/youtube.png",
       strongParagraph: "Nintendo Clone Website",
-      firstParagraph: "This is a clone of Nintendo's website. It is a single page that is built with HTML and CSS.",
-      link: "https://mock-nintendo.vercel.app/"
+      firstParagraph:
+        "This is a clone of Nintendo's website. It is a single page that is built with HTML and CSS.",
+      link: "https://mock-nintendo.vercel.app/",
     },
     {
       id: "modal6",
       h3: "Monstercat Clone",
       img: "./assets/img/monstercat.png",
       strongParagraph: "Monstercat Website Clone",
-      firstParagraph: "This is a clone of Monstercat's website. It is a single page that is built with HTML and CSS.",
-      link: "https://monstercat-clone-six.vercel.app/"
+      firstParagraph:
+        "This is a clone of Monstercat's website. It is a single page that is built with HTML and CSS.",
+      link: "https://monstercat-clone-six.vercel.app/",
     },
-    // {
-    //   id: "modal7",
-    //   h3: "UI Development",
-    //   img: "./assets/img/portfolio-7.jpg",
-    //   strongParagraph: "My first Awesome Website",
-    //   firstParagraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim corrupti repellendus sunt minus, ipsam cumque perspiciatis. Obcaecati, sit eligendi illo ipsa exercitationem modi cumque repellendus eos aliquam magni reiciendis nam?",
-    //   secondParagraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim corrupti repellendus sunt minus, ipsam cumque perspiciatis. Obcaecati, sit eligendi illo ipsa exercitationem modi cumque repellendus eos aliquam magni reiciendis nam?"
-    // },
-    // {
-    //   id: "modal8",
-    //   h3: "UI Development",
-    //   img: "./assets/img/portfolio-8.jpg",
-    //   strongParagraph: "My first Awesome Website",
-    //   firstParagraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim corrupti repellendus sunt minus, ipsam cumque perspiciatis. Obcaecati, sit eligendi illo ipsa exercitationem modi cumque repellendus eos aliquam magni reiciendis nam?",
-    //   secondParagraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim corrupti repellendus sunt minus, ipsam cumque perspiciatis. Obcaecati, sit eligendi illo ipsa exercitationem modi cumque repellendus eos aliquam magni reiciendis nam?"
-    // },
-  ]
-
-}
+    {
+      id: "modal7",
+      h3: "Coder Landing Page",
+      img: "./assets/img/coderlandingpage.png",
+      strongParagraph: "Landing Page",
+      firstParagraph:
+        "This is a landing page that has responsive CSS.  It is a single page that is built with HTML and CSS that is designed to drive conversion rates.",
+      link: "https://coder-landing-page.vercel.app",
+    },
+    {
+      id: "modal8",
+      h3: "Tetris",
+      img: "./assets/img/tetris.png",
+      strongParagraph: "Tetris Game",
+      firstParagraph: "A Tetris clone done with HTML, CSS, and JavaScript",
+      link: "https://tetris-alpha-coral.vercel.app/",
+    },
+  ],
+};
 
 // Generate Portfolio Cards
-const portfolioPanel = document.querySelector('.portfolio-panel');
-const portfolioCard = 'portfolio-card';
-const portfolioCardBody = 'portfolio-card-body';
-const cardModal = 'card-modal';
+const portfolioPanel = document.querySelector(".portfolio-panel");
+const portfolioCard = "portfolio-card";
+const portfolioCardBody = "portfolio-card-body";
+const cardModal = "card-modal";
 
-
-
-for(let i = 0; i< data.dataPortfolioCards.length; i++){
-  let card = document.createElement('div');
+for (let i = 0; i < data.dataPortfolioCards.length; i++) {
+  let card = document.createElement("div");
   card.classList.add(portfolioCard);
-  card.innerHTML =
-  `<div   
+  card.innerHTML = `<div   
     ${data.dataPortfolioCards[i].attributes[0].name}="${data.dataPortfolioCards[i].attributes[0].value}"
     ${data.dataPortfolioCards[i].attributes[1].name}=${data.dataPortfolioCards[i].attributes[1].value}
     class="portfolio-card-body">
@@ -235,16 +238,15 @@ for(let i = 0; i< data.dataPortfolioCards.length; i++){
 }
 
 // Generate corresponding modals
-const main = document.querySelector('.site-wrapper');
-const modal = 'modal';
+const main = document.querySelector(".site-wrapper");
+const modal = "modal";
 
-for(let i = 0; i< data.dataModals.length; i++){
-  let modalElement = document.createElement('div');
+for (let i = 0; i < data.dataModals.length; i++) {
+  let modalElement = document.createElement("div");
   modalElement.classList.add(modal);
-  modalElement.setAttribute('id', data.dataModals[i].id);
-  modalElement.setAttribute('data-animation', "slideDown");
-  modalElement.innerHTML =
-  `<div class="modal-dialog">
+  modalElement.setAttribute("id", data.dataModals[i].id);
+  modalElement.setAttribute("data-animation", "slideDown");
+  modalElement.innerHTML = `<div class="modal-dialog">
     <header class="modal-header">
       <h3>${data.dataModals[i].h3}</h3>
       <i data-close class="fas fa-times"></i>
--- js/main.js
+++ js/main.js
@@ -1,59 +1,56 @@
-const open = 'open';
+const open = "open";
 const root = document.documentElement;
-const active = 'active';
+const active = "active";
 const setActive = (e, selector) => {
-  if (document.querySelector(`${selector}.${active}`) !== null){
+  if (document.querySelector(`${selector}.${active}`) !== null) {
     document.querySelector(`${selector}.${active}`).classList.remove(active);
     e.classList.add(active);
-  }else{
+  } else {
     e.classList.add(active);
   }
-}
+};
 /*Modal*/
-
 
 const modalOpen = "[data-open]";
 const modalClose = "[data-close]";
-const isVisible = 'is-visible';
+const isVisible = "is-visible";
 const openModal = document.querySelectorAll(modalOpen);
 const closeModal = document.querySelectorAll(modalClose);
-const cardButton = document.querySelectorAll('.fa-square-plus');
-
+const cardButton = document.querySelectorAll(".fa-square-plus");
 
 // Full site modal open buttons
-for(const el of openModal){
-  el.addEventListener('click', function() {
+for (const el of openModal) {
+  el.addEventListener("click", function () {
     const modalId = this.dataset.open;
     document.getElementById(modalId).classList.add(isVisible);
     event.preventDefault();
   });
 }
 
-
-for(const el of closeModal){
-  el.addEventListener('click', function(){
+for (const el of closeModal) {
+  el.addEventListener("click", function () {
     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
   });
 }
 
 // Portfolio
 
-const dataFilter = '[data-filter]';
+const dataFilter = "[data-filter]";
 const filterLink = document.querySelectorAll(dataFilter);
-const portfolioData = '[data-item]';
+const portfolioData = "[data-item]";
 const portfolioItems = document.querySelectorAll(portfolioData);
 
-for (const e of filterLink){
-  e.addEventListener('click', function(){
-    setActive(e, '.filter-link');
+for (const e of filterLink) {
+  e.addEventListener("click", function () {
+    setActive(e, ".filter-link");
     const filter = this.dataset.filter;
-    portfolioItems.forEach((card) =>{
-      if(filter==='all'){
-        card.style.display = 'block';
-      } else if(card.dataset.item === filter){
-        card.style.display = 'block';
-      } else{
-        card.style.display = 'none';
+    portfolioItems.forEach((card) => {
+      if (filter === "all") {
+        card.style.display = "block";
+      } else if (card.dataset.item === filter) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
       }
     });
   });
@@ -61,30 +58,28 @@ for (const e of filterLink){
 
 // Search bar logic
 
-const searchBox = document.querySelector('#search');
+const searchBox = document.querySelector("#search");
 
-searchBox.addEventListener('keyup', (e) => {
+searchBox.addEventListener("keyup", (e) => {
   const searchInput = e.target.value.toLowerCase().trim();
-  portfolioItems.forEach((card) =>{
-    if(card.dataset.item.includes(searchInput)){
+  portfolioItems.forEach((card) => {
+    if (card.dataset.item.includes(searchInput)) {
       card.style.display = "block";
-    }else{
-      card.style.display = 'none';
+    } else {
+      card.style.display = "none";
     }
-  })
-
-
-})
+  });
+});
 
 // Individual Modal
-document.addEventListener('click', (e) =>{
-  if(e.target === document.querySelector('.modal.is-visible')){
-    document.querySelector('.modal.is-visible').classList.remove(isVisible);
+document.addEventListener("click", (e) => {
+  if (e.target === document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
   }
-})
+});
 
-document.addEventListener('keyup', (e) =>{
-  if(e.key === 'Escape'){
-    document.querySelector('.modal.is-visible').classList.remove(isVisible);
+document.addEventListener("keyup", (e) => {
+  if (e.key === "Escape") {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
   }
 });
